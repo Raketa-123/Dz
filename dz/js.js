20d90ef44ad4b0cea6ac0d78953f42a7ef54a149
Vladislav Babenko <vlad.babenko.84@bk.ru>
2025-02-02 22:33:15 +0500

    Add files via upload

---
+++ dz/js.js
@@ -0,0 +1,14 @@
+// let word = prompt("Введите слово");
+// let encrypted = "";
+// let glasnie = "аеёиоуэюя";
+
+// for (let i = 0; i < word.length; i++) {
+//     if (glasnie.includes(word[i].toLowerCase())) {
+//         encrypted += word[i] + "наггетс";
+//     } 
+//     else {
+//         encrypted += word[i];
+//     }
+// }
+
+// alert(encrypted);
